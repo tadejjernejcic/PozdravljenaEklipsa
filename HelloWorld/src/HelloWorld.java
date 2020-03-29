ff2a151e6920e997ea6bb78e6e10373ed9913f38
tadejjernejcic <ohgodwhatanemail@gmail.com>
2020-03-29 20:01:11 +0200

    sprememba za github test

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -98,6 +98,7 @@ public class HelloWorld {
         }
         catch(Exception e) {
             System.err.println("Napaka: " + e);
+            System.err.println("Sprememba za github");
         }
 
 
